19361256dd8150198fc3c9682b536e60ce117485
José Carlos Gutiérrez <joseca20025@gmail.com>
2025-11-24 00:45:08 -0400

    add: crea archivo db_destino.py para la conexión a la base de datos con configuración de entorno

---
+++ etl-python/db_destino.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+mariadb_connection_string = os.getenv("DB_CONNECTION_URL")
+
+if not mariadb_connection_string:
+    raise ValueError("DB_CONNECTION_URL environment variable not set")
+
+MARIADB_CONNECTION_STRING = mariadb_connection_string
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "test_db")
+
+MARIADB_CONNECTION = pymysql.connect(
+    host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME
+)
